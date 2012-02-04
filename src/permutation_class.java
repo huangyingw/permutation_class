e6fac1f3df1d04ab0d0a66a0fbecf8da24fcdc0d
huangyingw@linux_programming <huangyingw@gmail.com>
2012-02-04 18:40:48 +0800

    n

--- src/permutation_class.java
+++ src/permutation_class.java
@@ -1,7 +1,8 @@
 import java.util.ArrayList;
 
 public class permutation_class {
-	public static ArrayList<int[]> permutation(ArrayList<Integer> arr) {
-		return new ArrayList<int[]>(arr.toArray(new int[arr.size()]));
+	public static void permutation(ArrayList<Integer> arr) {
+		ArrayList<Integer[]> result = new ArrayList<Integer[]>();
+		result.add(arr.toArray());
 	}
 }
