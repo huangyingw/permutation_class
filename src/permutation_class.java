e9775356be88798892fbcd93231f3eea1900b1ea
huangyingw@linux_programming <huangyingw@gmail.com>
2012-02-06 20:51:18 +0800

    n

--- src/permutation_class.java
+++ src/permutation_class.java
@@ -9,7 +9,7 @@ public class permutation_class {
 		} else {
 			for (int i = 0; i < orginal.length(); i++) {
 				String result = "";
-				String resultA = result + orginal.charAt(i);
+				String resultA = "" + orginal.charAt(i);
 				String leftS = orginal.substring(0, i)
 						+ orginal.substring(i + 1, orginal.length());
 				for (String element : permutation(leftS)) {
